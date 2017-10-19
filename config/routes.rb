f4760e5077db048298170f9a0f9cd0c756aa3a67
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2017-10-19 15:08:27 -0700

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :channels
   resources :homepages
 
-  root to: 'homepage#index'
+  root to: 'homepages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
