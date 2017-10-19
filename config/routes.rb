129759267368ac412fe6a86716bd32423a042a8f
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2017-10-19 15:04:56 -0700

    ADDED POSTGRES

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :channels
   resources :homepages
+
+  root to: 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
