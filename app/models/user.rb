39aeb6dad1c368d63e173752e9ad7c6767f24d97
edwardsanchez001 <jesus.ed.sanchez@gmail.com>
2017-11-02 16:18:29 -0500

    added a icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  #has_secure_password
+
+  validates :username, presence: true, length: {minimum: 5}, uniqueness: { case_sensitve: false}
+  validates :password, length: {minimum: 8}
   has_many :post
   has_many :comment
 end
