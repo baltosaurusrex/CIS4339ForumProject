d85f752040e941ca03883aa2a09c10cccb4e59ef
Geleela <gmmenigistu@uh.edu>
2017-11-07 18:08:28 -0600

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-
-  get 'pages/home'
-
-  root to: 'pages#home'
+  resources :sessions, only: [:new, :create]
   resources :users, only: [:new, :create]
-  resources :comments
-  resources :posts
-  resources :channels
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
