df0b59a7e2ddb8094339894f16382862181f2f94
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2017-11-14 12:17:52 -0800

    FIXED ALL THE STUFF THAT WAS DELETED

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
-resources :sessions, only: [:new, :create] do
+Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/home'
+
+  root to: 'pages#home'
+
+  resources :comments
+  resources :posts
+  resources :channels
+  resources :sessions, only: [:new, :create]
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create] do
   delete :destroy, on: :collection
-end
+
+
+
+  end
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
