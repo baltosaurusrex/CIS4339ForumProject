ee3c7e867bd3f5bbc6d1db1abbd5e5858dcc1005
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2017-11-16 14:39:49 -0800

    wip changes to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  #before_action :authenticate_user!
+  
   has_many :posts
   has_many :comments
 end
