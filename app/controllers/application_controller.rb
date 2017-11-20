bbae5f39340a023df11f497d357edffc29213e06
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2017-11-20 12:45:23 -0800

    GOT LOGIN WORKING.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
     session[:user_id] = user.id
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id])
+  # end
 
   def logout
     session.delete(:user_id)
