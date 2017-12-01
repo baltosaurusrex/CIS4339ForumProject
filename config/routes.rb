d1b28816f25b4178c60a8bd3757a34d84cd73c7d
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2017-12-01 10:29:59 -0800

    grrr why did logout break

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }
+  resources :users do
+    collection { post :import}
+  end
+
   post 'posts/search' => 'posts#search', as: 'search_posts'
   get 'all_post_updates/:id' => 'post_updates#index', as: 'all_post_updates'
   get 'post_updates/new/:id' => 'post_updates#new', as: 'new_post_update'
@@ -13,19 +20,12 @@ Rails.application.routes.draw do
   get 'all_channel_updates/:id' => 'chanel_updates#index', as: 'all_channel_updates'
   get 'channel_updates/new/:id' => 'channel_updates#new', as: 'new_channel_update'
 
-
   post 'comments/search' => 'comments#search', as: 'search_comments'
   get 'all_comment_updates/:id' => 'comment_updates#index', as: 'all_comment_updates'
   get 'comment_updates/new/:id' => 'comment_updates#new', as: 'new_comment_update'
   get 'pages/home'
 
   get 'graph' => 'graphs#show', as: 'reports'
-   devise_for :users, controllers: {
-      sessions: 'users/sessions'
-  }
-  resources :users do
-    collection { post :import}
-  end
 
   resources :comments
   resources :posts
